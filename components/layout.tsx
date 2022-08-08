ad681085f11944fb031fb9546d79c6111c82e400
cemarv25 <cesar.martinez0010@outlook.com>
2022-08-08 15:54:41 -0600

    added responsiveness to the navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import NavLink from './navlink';
 import styles from '../styles/layout.module.css';
-import { Button } from 'antd';
+import { Button, Col, Row, Typography } from 'antd';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -13,38 +13,57 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <nav className={styles.navbar}>
-        <div className={styles.brand}>
-          <Link href="/">
-            <a className={styles.logo}>
-              <Image
-                layout="responsive"
-                width={70}
-                height={70}
-                src="/dummy.png"
-                alt="logo"
-                style={{ borderRadius: '50%' }}
-              />
-            </a>
-          </Link>
-          <Link href="/">
-            <a>
-              <span className={styles.name}>Reviews</span>
-            </a>
-          </Link>
-        </div>
-        <ul className={styles.navlinks}>
-          <li>
-            <NavLink exact href="/restaurants">
-              Restaurants
-            </NavLink>
-          </li>
-          <li>
-            <NavLink exact href="/dishes">
-              Dishes
-            </NavLink>
-          </li>
-        </ul>
-        <Button type="primary">Log In</Button>
+        <Row justify="center" align="middle">
+          <Col xs={18} sm={9} lg={10}>
+            <Row align="middle" gutter={2}>
+              <Col xs={4} sm={3} lg={2}>
+                <Link href="/">
+                  <a>
+                    <Image
+                      layout="responsive"
+                      width={70}
+                      height={70}
+                      src="/dummy.png"
+                      alt="logo"
+                      style={{ borderRadius: '50%' }}
+                    />
+                  </a>
+                </Link>
+              </Col>
+              <Col flex="auto">
+                <Link href="/">
+                  <a>
+                    <span className={styles.name}>Reviews</span>
+                  </a>
+                </Link>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={0} sm={12} lg={11}>
+            <ul className={styles.navlinks}>
+              <li>
+                <NavLink exact href="/restaurants">
+                  Restaurants
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact href="/dishes">
+                  Dishes
+                </NavLink>
+              </li>
+            </ul>
+          </Col>
+          <Col xs={0} sm={3}>
+            <Row justify="end">
+              <Button type="primary">Log In</Button>
+            </Row>
+          </Col>
+          <Col xs={6} sm={0}>
+            <Row justify="end">
+              <Typography>Menu</Typography>
+            </Row>
+          </Col>
+        </Row>
       </nav>
       {children}
     </>
